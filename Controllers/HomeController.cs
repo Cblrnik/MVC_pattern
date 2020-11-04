73fdd7672c3177b2886f205f47e7fe71e86e75ee
Cblrnik <rnehaikov@gmail.com>
2020-11-04 15:35:41 +0300

    Last update. Work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
 
 namespace MVC1.Controllers
 {
     public class HomeController : Controller
     {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
+            return View(db.Phones.ToList());
+        }
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
             return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
         }
     }
 }
